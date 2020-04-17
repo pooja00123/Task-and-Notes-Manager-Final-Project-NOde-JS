bc9decbcf25320f38783f2f9ec860f649b1e68a4
Pooja <pooja02@nagarro.com>
2020-04-17 07:04:39 +0530

    Correcting node version and port

--- FinalAssignment_TODOS/FinalAssignment_TODOS/server.js
+++ FinalAssignment_TODOS/FinalAssignment_TODOS/server.js
@@ -2,13 +2,15 @@ const express = require('express')
 const { db } = require('./db')
 const todoRoute = require('./routes/todos')
 const noteRoute = require('./routes/notes')
-const app = express()
-port = process.env.PORT || 5000
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+port = process.env.PORT || 5000
+const app = express()
+
 app.use('/Assets', express.static(__dirname + 'Assets'))
 app.use('/', express.static(__dirname + '/public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 
 app.use('/todos',todoRoute)
 app.use('/notes',noteRoute)
